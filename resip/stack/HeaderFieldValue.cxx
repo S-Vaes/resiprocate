e51349f566f998650b62502313cf1727b54c6d67
svaes <sil.vaes@rideontrack.com>
2024-11-06 11:02:31 +0100

    testing.

--- resip/stack/HeaderFieldValue.cxx
+++ resip/stack/HeaderFieldValue.cxx
@@ -29,15 +29,17 @@ HeaderFieldValue::HeaderFieldValue(const char* field, unsigned int fieldLength)
 {}
 
 HeaderFieldValue::HeaderFieldValue(const HeaderFieldValue& hfv)
-   : mField(0),
+   : mField(nullptr),
      mFieldLength(hfv.mFieldLength),
      mMine(true)
 {
    if(mFieldLength)
    {
       char* newField = new char[mFieldLength];
-      memcpy(newField, hfv.mField, mFieldLength);
-      mField=newField;
+      if(hfv.mField != nullptr) {
+         memcpy(newField, hfv.mField, mFieldLength);
+         mField=newField;
+      }
    }
 }
 
@@ -60,11 +62,11 @@ HeaderFieldValue::operator=(const HeaderFieldValue& rhs)
          mField=0;
       }
    }
-   
+
    return *this;
 }
 
-HeaderFieldValue& 
+HeaderFieldValue&
 HeaderFieldValue::copyWithPadding(const HeaderFieldValue& rhs)
 {
    if(this!=&rhs)
@@ -83,7 +85,7 @@ HeaderFieldValue::copyWithPadding(const HeaderFieldValue& rhs)
          mField=0;
       }
    }
-   
+
    return *this;
 }
 
@@ -125,7 +127,7 @@ HeaderFieldValue::~HeaderFieldValue()
   }
 }
 
-EncodeStream& 
+EncodeStream&
 HeaderFieldValue::encode(EncodeStream& str) const
 {
    str.write(mField, mFieldLength);
@@ -139,22 +141,22 @@ EncodeStream& resip::operator<<(EncodeStream& stream, HeaderFieldValue& hfv)
 }
 
 /* ====================================================================
- * The Vovida Software License, Version 1.0 
- * 
+ * The Vovida Software License, Version 1.0
+ *
  * Copyright (c) 2000 Vovida Networks, Inc.  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
  *    the documentation and/or other materials provided with the
  *    distribution.
- * 
+ *
  * 3. The names "VOCAL", "Vovida Open Communication Application Library",
  *    and "Vovida Open Communication Application Library (VOCAL)" must
  *    not be used to endorse or promote products derived from this
@@ -164,7 +166,7 @@ EncodeStream& resip::operator<<(EncodeStream& stream, HeaderFieldValue& hfv)
  * 4. Products derived from this software may not be called "VOCAL", nor
  *    may "VOCAL" appear in their name, without prior written
  *    permission of Vovida Networks, Inc.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
@@ -178,9 +180,9 @@ EncodeStream& resip::operator<<(EncodeStream& stream, HeaderFieldValue& hfv)
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
- * 
+ *
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by Vovida
  * Networks, Inc. and many individuals on behalf of Vovida Networks,
  * Inc.  For more information on Vovida Networks, Inc., please see
