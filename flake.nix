1aa7793fad0e89744a61fee9d364e8a2c4612b81
Sil vaes <sil.vaes@rideontrack.com>
2025-01-21 09:23:40 +0100

    Added darwin packages.

--- flake.nix
+++ flake.nix
@@ -1,77 +1,57 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux =
-      let
-        # Helper function to get packages with specified stdenv
-        getPkgs = stdenv: let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          cpppkgs = pkgs;  # You might want to customize this based on the stdenv
-        in {
-          inherit pkgs cpppkgs stdenv;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # Helper to create packages for a specific target platform's pkgs
+      mkPkgsFor = targetPkgs: rec {
+        qpid-proton = targetPkgs.callPackage ./nix/qpid-proton {};
+        cajun = targetPkgs.callPackage ./nix/cajun {};
+        sipxtapi = targetPkgs.callPackage ./nix/sipxtapi {};
+        resiprocate = targetPkgs.callPackage ./default.nix {
+          inherit (targetPkgs.unixtools) xxd;
+          inherit sipxtapi qpid-proton cajun;
+          libdb = targetPkgs.db62;
         };
-
-        # Define package builders
-        qpid-proton = { stdenv, pkgs, cpppkgs }: pkgs.callPackage ./nix/qpid-proton {
-          inherit stdenv;
-        };
-
-        cajun = { stdenv, pkgs, cpppkgs }: pkgs.callPackage ./nix/cajun {
-          inherit stdenv;
-        };
-
-        sipxtapi = { stdenv, pkgs, cpppkgs }: pkgs.callPackage ./nix/sipxtapi {
-          inherit stdenv;
-        };
-
-        resiprocate = { stdenv, pkgs, cpppkgs }: pkgs.callPackage ./default.nix {
-          inherit stdenv;
-          inherit (pkgs.unixtools) xxd;
-          sipxtapi = (sipxtapi { inherit stdenv pkgs cpppkgs; });
-          qpid-proton = (qpid-proton { inherit stdenv pkgs cpppkgs; });
-          cajun = (cajun { inherit stdenv pkgs cpppkgs; });
-          libdb = cpppkgs.db62;
-        };
-
-        # Get package sets for different compilers
-        gccPkgs = getPkgs nixpkgs.legacyPackages.x86_64-linux.gcc14Stdenv;
-        clangPkgs = getPkgs nixpkgs.legacyPackages.x86_64-linux.llvmPackages_19.libcxxStdenv;
-
-        # Build variants with different stdenvs
-        mkVariant = pkgSet: {
-          resiprocate = resiprocate {
-            stdenv = pkgSet.stdenv;
-            pkgs = pkgSet.pkgs;
-            cpppkgs = pkgSet.cpppkgs;
-          };
-          qpid-proton = qpid-proton {
-            stdenv = pkgSet.stdenv;
-            pkgs = pkgSet.pkgs;
-            cpppkgs = pkgSet.cpppkgs;
-          };
-          cajun = cajun {
-            stdenv = pkgSet.stdenv;
-            pkgs = pkgSet.pkgs;
-            cpppkgs = pkgSet.cpppkgs;
-          };
-          sipxtapi = sipxtapi {
-            stdenv = pkgSet.stdenv;
-            pkgs = pkgSet.pkgs;
-            cpppkgs = pkgSet.cpppkgs;
-          };
-        };
-
-      in
-      {
-        default = (mkVariant gccPkgs).resiprocate;
 
         # GCC variants
-        gccResiprocate = (mkVariant gccPkgs).resiprocate;
+        gccQpidProton = qpid-proton.override { stdenv = targetPkgs.gcc14Stdenv; };
+        gccCajun = cajun.override { stdenv = targetPkgs.gcc14Stdenv; };
+        gccSipxtapi = sipxtapi.override { stdenv = targetPkgs.gcc14Stdenv; };
+        gccResiprocate = resiprocate.override { stdenv = targetPkgs.gcc14Stdenv; };
+
         # Clang variants
-        clangResiprocate = (mkVariant clangPkgs).resiprocate;
+        clangQpidProton = qpid-proton.override { stdenv = targetPkgs.llvmPackages_19.libcxxStdenv; };
+        clangCajun = cajun.override { stdenv = targetPkgs.llvmPackages_19.libcxxStdenv; };
+        clangSipxtapi = sipxtapi.override { stdenv = targetPkgs.llvmPackages_19.libcxxStdenv; };
+        clangResiprocate = resiprocate.override { stdenv = targetPkgs.llvmPackages_19.libcxxStdenv; };
       };
-  };
+    in {
+      packages = rec {
+        # Native packages
+        native = mkPkgsFor pkgs;
+        default = native.resiprocate;
+
+        # Cross-compiled packages for Darwin
+        aarch64-darwin = mkPkgsFor crossPkgs.aarch64-darwin;
+        x86_64-darwin = mkPkgsFor crossPkgs.x86_64-darwin;
+      };
+
+      overlays.default = final: prev: {
+        inherit (self.packages.${system})
+          qpid-proton
+          cajun
+          sipxtapi
+          resiprocate;
+      };
+    });
 }
